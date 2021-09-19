e9359bbca19a62acbf4a9df5fb050d41ff5801b7
Sam King <mail@samking.co>
2021-09-19 20:30:38 +0100

    Add social meta tags

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,30 @@ import Head from "next/head";
 import styles from "@components/App.module.css";
 
 export function App({ Component, pageProps }: AppProps) {
+  const title = "Roots by Sam King";
+  const description =
+    "A collection of 40 photographs exploring my roots in the Scottish Highlands. Available to purchase on OpenSea.";
+  const url = "https://roots.samking.co";
+  const socialImage = `${url}/social.jpg`;
+
   return (
     <>
       <Head>
-        <title>Roots by Sam King</title>
-        {/* TODO: Add description etc */}
-        <meta name="description" content="A thing about a thing" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={socialImage} />
+        <meta property="og:url" content={url} />
+        <meta property="og:site_name" content={title} />
+
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={socialImage} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@samkingco" />
       </Head>
       <div className={styles.content}>
         <Component {...pageProps} />
