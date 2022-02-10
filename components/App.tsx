ebea5e07c9355f703390983e350a1953daa9bba3
Sam King <mail@samking.co>
2022-02-10 21:09:21 +0000

    Add plausible tracking

--- components/App.tsx
+++ components/App.tsx
@@ -4,8 +4,17 @@ import styles from "components/App.module.css";
 
 export function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.content}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <script
+          defer
+          data-domain="roots.samking.photo"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </Head>
+      <div className={styles.content}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
